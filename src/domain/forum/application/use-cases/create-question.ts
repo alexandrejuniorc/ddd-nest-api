b7b1ce99f54526e43c8597adb92b4d6f12ce8bf1
Alexandre Junior <alejunior.dev@gmail.com>
2025-06-10 22:58:47 -0300

    feat: add communication between layers

--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -1,9 +1,12 @@
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
-import { Question } from '../../enterprise/entities/question'
-import { QuestionsRepository } from '../repositories/questions-repository'
-import { Either, right } from '@/core/either'
-import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
-import { QuestionAttachmentList } from '../../enterprise/entities/question-attachment-list'
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
+import { Question } from "../../enterprise/entities/question"
+import { QuestionsRepository } from "../repositories/questions-repository"
+import { Either, right } from "@/core/either"
+import { QuestionAttachment } from "../../enterprise/entities/question-attachment"
+import { QuestionAttachmentList } from "../../enterprise/entities/question-attachment-list"
+
+// EXTERNAL DEPENDENCY
+import { Injectable } from "@nestjs/common"
 
 interface CreateQuestionUseCaseRequest {
   authorId: string
@@ -19,6 +22,7 @@ type CreateQuestionUseCaseResponse = Either<
   }
 >
 
+@Injectable()
 export class CreateQuestionUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
 
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,9 +1,10 @@
-import { Module } from "@nestjs/common";
-import { CreateAccountController } from "./controllers/create-account.controller";
-import { AuthenticateController } from "./controllers/authenticate.controller";
-import { CreateQuestionController } from "./controllers/create-question.controller";
-import { FetchRecentQuestionsController } from "./controllers/fetch-recent-questions.controller";
-import { DatabaseModule } from "../database/database.module";
+import { Module } from "@nestjs/common"
+import { CreateAccountController } from "./controllers/create-account.controller"
+import { AuthenticateController } from "./controllers/authenticate.controller"
+import { CreateQuestionController } from "./controllers/create-question.controller"
+import { FetchRecentQuestionsController } from "./controllers/fetch-recent-questions.controller"
+import { DatabaseModule } from "../database/database.module"
+import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question"
 
 @Module({
   imports: [DatabaseModule],
@@ -13,5 +14,6 @@ import { DatabaseModule } from "../database/database.module";
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
+  providers: [CreateQuestionUseCase],
 })
-export class HttpModule { }
+export class HttpModule {}

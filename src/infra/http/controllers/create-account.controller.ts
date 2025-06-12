8fe45d1b05396a1041839624c1cb204f4515341d
Alexandre Junior <alejunior.dev@gmail.com>
2025-06-12 11:03:36 -0300

    refactor: handle with controller errors

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -1,7 +1,16 @@
-import { HttpCode, UsePipes, Body, Controller, Post } from "@nestjs/common"
+import {
+  HttpCode,
+  UsePipes,
+  Body,
+  Controller,
+  Post,
+  ConflictException,
+  BadRequestException,
+} from "@nestjs/common"
 import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe"
 import { z } from "zod"
 import { RegisterStudentUseCase } from "@/domain/forum/application/use-cases/register-student"
+import { StudentAlreadyExistsError } from "@/domain/forum/application/use-cases/errors/student-already-exists.error"
 
 const createAccountBodySchema = z.object({
   name: z.string(),
@@ -28,7 +37,14 @@ export class CreateAccountController {
     })
 
     if (result.isLeft()) {
-      throw new Error()
+      const error = result.value
+
+      switch (error.constructor) {
+        case StudentAlreadyExistsError:
+          throw new ConflictException(error.message)
+        default:
+          throw new BadRequestException()
+      }
     }
   }
 }

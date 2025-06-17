f3404c10e51cb9797014ed302f748de0ee3de17a
Alexandre Junior <alejunior.dev@gmail.com>
2025-06-16 22:39:46 -0300

    feat: add EditQuestionController and tests

--- src/infra/database/prisma/repositories/prisma-answers.repository.ts
+++ src/infra/database/prisma/repositories/prisma-answers.repository.ts
@@ -6,7 +6,7 @@ import { PrismaService } from "../prisma.service"
 import { PrismaAnswerMapper } from "../mappers/prisma-answer.mapper"
 
 @Injectable()
-export class PrismaAnswerRepository implements AnswersRepository {
+export class PrismaAnswersRepository implements AnswersRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(answer: Answer): Promise<void> {

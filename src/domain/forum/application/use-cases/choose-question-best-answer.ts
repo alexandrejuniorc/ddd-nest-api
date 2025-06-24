01e4a53bde022cdcbca5a1af080a06c4860925cc
Alexandre Junior <alejunior.dev@gmail.com>
2025-06-23 22:48:19 -0300

    feat: implement ChooseQuestionBestAnswer use case and controller with E2E tests

--- src/domain/forum/application/use-cases/choose-question-best-answer.ts
+++ src/domain/forum/application/use-cases/choose-question-best-answer.ts
@@ -1,9 +1,10 @@
-import { AnswersRepository } from '../repositories/answers-repository'
-import { Question } from '../../enterprise/entities/question'
-import { QuestionsRepository } from '../repositories/questions-repository'
-import { Either, left, right } from '@/core/either'
-import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
-import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+import { AnswersRepository } from "../repositories/answers-repository"
+import { Question } from "../../enterprise/entities/question"
+import { QuestionsRepository } from "../repositories/questions-repository"
+import { Either, left, right } from "@/core/either"
+import { ResourceNotFoundError } from "@/core/errors/errors/resource-not-found-error"
+import { NotAllowedError } from "@/core/errors/errors/not-allowed-error"
+import { Injectable } from "@nestjs/common"
 
 interface ChooseQuestionBestAnswerUseCaseRequest {
   authorId: string
@@ -16,6 +17,7 @@ type ChooseQuestionBestAnswerUseCaseResponse = Either<
   }
 >
 
+@Injectable()
 export class ChooseQuestionBestAnswerUseCase {
   constructor(
     private questionsRepository: QuestionsRepository,

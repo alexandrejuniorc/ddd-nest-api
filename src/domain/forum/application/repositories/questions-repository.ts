77df0dc74c2b9eb7823107d50c7e7edf2d6da711
Alexandre Junior <alejunior.dev@gmail.com>
2025-07-09 15:42:13 -0300

    feat: add support for fetching question comments with author details and update related repositories

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -2,10 +2,10 @@ import { PaginationParams } from "@/core/repositories/pagination-params"
 import { Question } from "../../enterprise/entities/question"
 
 export abstract class QuestionsRepository {
+  abstract findById(questionId: string): Promise<Question | null>
+  abstract findBySlug(slug: string): Promise<Question | null>
+  abstract findManyRecent(params: PaginationParams): Promise<Question[]>
   abstract create(question: Question): Promise<void>
   abstract save(question: Question): Promise<void>
-  abstract findBySlug(slug: string): Promise<Question | null>
-  abstract findById(questionId: string): Promise<Question | null>
-  abstract findManyRecent(params: PaginationParams): Promise<Question[]>
   abstract delete(question: Question): Promise<void>
 }
--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,15 +1,15 @@
-import { PaginationParams } from '@/core/repositories/pagination-params'
-import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
-import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+import { PaginationParams } from "@/core/repositories/pagination-params"
+import { QuestionCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository"
+import { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment"
+import { CommentWithAuthor } from "@/domain/forum/enterprise/entities/value-objects/comment-with-author"
+import { InMemoryStudentsRepository } from "./in-memory-students-repository"
 
 export class InMemoryQuestionCommentsRepository
   implements QuestionCommentsRepository
 {
-  public items: QuestionComment[] = []
+  constructor(private studentsRepository: InMemoryStudentsRepository) {}
 
-  async create(questionComment: QuestionComment) {
-    this.items.push(questionComment)
-  }
+  public items: QuestionComment[] = []
 
   async findById(id: string) {
     const questionComment = this.items.find((item) => item.id.toString() === id)
@@ -30,6 +30,41 @@ export class InMemoryQuestionCommentsRepository
       .slice((page - 1) * 20, page * 20)
 
     return questionComments
+  }
+
+  async findManyByQuestionIdWithAuthor(
+    questionId: string,
+    { page }: PaginationParams,
+  ) {
+    const questionComments = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+      .map((comment) => {
+        const author = this.studentsRepository.items.find((student) => {
+          return student.id.equals(comment.authorId)
+        })
+
+        if (!author) {
+          throw new Error(
+            `Author with ID ${comment.authorId.toString()} does not exist.`,
+          )
+        }
+
+        return CommentWithAuthor.create({
+          commentId: comment.id,
+          authorId: comment.authorId,
+          authorName: author?.name,
+          content: comment.content,
+          createdAt: comment.createdAt,
+          updatedAt: comment.updatedAt,
+        })
+      })
+
+    return questionComments
+  }
+
+  async create(questionComment: QuestionComment) {
+    this.items.push(questionComment)
   }
 
   async delete(questionComment: QuestionComment) {

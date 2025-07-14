282c2fe0c628c081e454618dff48e7dba62cbe4e
Alexandre Junior <alejunior.dev@gmail.com>
2025-07-13 22:27:19 -0300

    feat: implements cache with redis

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -13,6 +13,7 @@ export const envSchema = z.object({
   REDIS_HOST: z.string().optional().default("127.0.0.1"),
   REDIS_PORT: z.coerce.number().optional().default(6379),
   REDIS_DB: z.coerce.number().optional().default(0),
+  REDIS_PASSWORD: z.string().optional().default("redis"),
 })
 
 export type Env = z.infer<typeof envSchema>
